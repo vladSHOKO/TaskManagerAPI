c09c4bef54cbcfe34e8e264e5ccd665765e86be5
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-07-14 15:54:30 +0300

    Add TaskCollectionDataProviderTest

---
+++ tests/DataProvider/TaskCollectionDataProviderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\DataProvider;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TaskCollectionDataProviderTest extends WebTestCase
+{
+    public function testProvideAsAdmin()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/login',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: json_encode(['username' => 'test@admin.ru', 'password' => 'pass']),
+        );
+
+        $response = $client->getResponse();
+
+        $content = $response->getContent();
+
+        $token = json_decode($content, true)['token'];
+
+        $client->setServerParameters(['HTTP_AUTHORIZATION' => 'Bearer ' . $token]);
+
+        $client->request('GET', '/api/tasks');
+
+        $this->assertResponseIsSuccessful();
+
+    }
+
+}
