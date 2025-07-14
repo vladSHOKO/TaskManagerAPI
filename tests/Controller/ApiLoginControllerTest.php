aadb3662f85098599165d6cdcb9ff082fdc838f0
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-07-14 14:38:55 +0300

    Add ApiLoginControllerTest

---
+++ tests/Controller/ApiLoginControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApiLoginControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    public function testLoginFail(): void
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/login');
+
+        $this->assertResponseStatusCodeSame(401);
+    }
+
+    public function testLoginSuccess(): void
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/login',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: json_encode(['username' => 'test@user.ru', 'password' => 'pass']),
+        );
+
+        $this->assertResponseIsSuccessful();
+    }
+}
