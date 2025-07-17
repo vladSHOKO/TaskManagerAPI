d4e1421b8fa3b8ec80b9a57c962c295ca1d217be
Vladislav <131362319+vladSHOKO@users.noreply.github.com>
2025-07-17 18:10:53 +0300

    Run code sniffer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -161,7 +161,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __serialize(): array
     {
         $data = (array) $this;
-        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+        $data["\0" . self::class . "\0password"] = hash('crc32c', $this->password);
 
         return $data;
     }
